1919b3cb13e38936f2d37f3dfca2d8870f05c97f
Choyan7 <75743622+Choyan7@users.noreply.github.com>
2021-08-06 16:40:47 +0600

    Add files via upload

---
+++ js/glide.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    
+var glide = new Glide('#intro', {
+  type: 'carousel',
+  perView: 4,
+  focusAt: 'center',
+  breakpoints: {
+    800: {
+      perView: 2
+    },
+    480: {
+      perView: 1
+    }
+  }
+})
+
+glide.mount()
+
+
+
+
+
+
+
+
+});
